6d3df1281ee1cee876788ea772149bec0bb70ce1
Jak-Kob <jak.kobiela@gmail.com>
2020-09-14 10:20:41 +0200

    init dev

--- VirtualHome/VirtualHome/Program.cs
+++ VirtualHome/VirtualHome/Program.cs
@@ -4,6 +4,13 @@ namespace VirtualHome
 {
     class Program
     {
+        enum Room
+        {
+            Kitchen,
+            Bedroom,
+            LivingRoom            
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
